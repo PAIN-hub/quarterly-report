6949b5d4d4b11c7c96411c4daa47fd0d0f204ba8
ƤȺIƝ <163692189+PAIN-hub@users.noreply.github.com>
2026-05-02 08:38:29 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -154,11 +154,12 @@ SIMPLE_JWT = {
 # CORS
 # ========================
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5500",
     "http://127.0.0.1:5500",
+    'https://mda-backend-3exm.onrender.com',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
